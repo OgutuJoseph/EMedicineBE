74439f561ed4286b0b51f2ad24ad59d3f5bfccf4
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-08 10:56:59 +0300

    Registration API

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using EMedicineBE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace EMedicineBE.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+			// used to fetch connection string
+            _configuration = configuration;
+        }
+
+		[HttpPost]
+		[Route("registration")]
+		public Response register(Users users)
+		{
+			Response response = new Response();
+			DAL dal = new DAL();
+			SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("EMedCS").ToString());
+			response = dal.register(users, connection);
+
+			return response;
+		}
+    }
+}
