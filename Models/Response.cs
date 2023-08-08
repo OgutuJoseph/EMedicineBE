bec1bc742137907574986209e1f993ce71b95622
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-08 10:27:02 +0300

    Created Order and OrderItem models

--- Models/Response.cs
+++ Models/Response.cs
@@ -7,11 +7,24 @@ namespace EMedicineBE.Models
 		public int StatusCode { get; set; }
 		public string StatusMessage { get; set; }
 
+		// Users
 		public List<Users> listUsers { get; set; }
 		public Users user { get; set; }
+
+		// Medicines
 		public List<Medicines> listMedicines { get; set; }
 		public Medicines medicine { get; set; }
 
+		// Cart
 		public List<Cart> listCart { get; set; }
+
+		// Orders
+		public List<Orders> listOrders { get; set; }
+		public Orders order { get; set; }
+
+		// Order Items
+		public List <OrderItems> listItems { get; set; }
+		public OrderItems orderItem { get; set; }
+
 	}
 }
