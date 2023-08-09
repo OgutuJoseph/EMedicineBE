88d25ade509645e1d9e1f9aa6ff31b2fa74cad06
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-09 09:03:42 +0300

    Update Cart model

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -4,6 +4,7 @@
 	{
 		public int ID { get; set; }
         public int UserId { get; set; }
+        public int MedicineId { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
         public int Quantity { get; set; }
