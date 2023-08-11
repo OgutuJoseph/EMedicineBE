dfe5c37002d532f33e503843b4d39a6c8824e513
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-11 14:06:46 +0300

    Added Resources folder

---
+++ Resources/DBSchema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE EMedicine;
+Use EMedicine;
+
+CREATE TABLE Users(ID INT IDENTITY(1,1) PRIMARY KEY, FirstName VARCHAR(100), LastName VARCHAR(100), Password VARCHAR(100), 
+Email VARCHAR(100), Fund DECIMAL (18,2), Type VARCHAR(100), Status INT, CreatedOn DateTime);
+
+CREATE TABLE Medicines(ID INT IDENTITY(1,1) PRIMARY KEY, Name VARCHAR(100), Manufacturer VARCHAR(100), UnitPrice DECIMAL(18,2),
+Discount DECIMAL (18,2), Quantity INT, ExpDate DATETIME, ImageURL VARCHAR(100), Status INT);
+
+CREATE TABLE Cart(ID INT IDENTITY(1,1) PRIMARY KEY, UserId INT, MedicineId	INT, UnitPrice DECIMAL(18,2), Discount DECIMAL(18,2),
+Quantity INT, TotalPrice DECIMAL(18,2));
+
+CREATE TABLE Orders(ID INT IDENTITY(1,1) PRIMARY KEY, UserId INT, OrderNo VARCHAR(100), OrderTotal DECIMAL(18,2), OrderStatus VARCHAR(100));
+
+CREATE TABLE OrderItems(ID INT IDENTITY(1,1) PRIMARY KEY, OrderId INT, MedicineId INT, UnitPrice DECIMAL(18,2), Quantity INT, TotalPrice DECIMAL(18,2))
+
